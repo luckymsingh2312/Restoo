6f1aec3d631c774ea0db6b9ae143ba34763f97b7
Anjali Singh <95577810+luckymsingh2312@users.noreply.github.com>
2024-07-20 01:00:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// Swiper js
+
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".home", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+
+    menu.onclick = () => {
+        menu.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+    };
+
+    window.onscroll = () => {
+        menu.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    };
+
+});
+
+
+
+
+console.log("Hello world!");
